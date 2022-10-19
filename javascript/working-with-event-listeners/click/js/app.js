7a3736b43882f67a839c60af2c7e400e027e9c34
Roy A Vanegas <roy@thecodeeducators.com>
2022-10-19 12:31:52 -0400

    Add click event listener example

---
+++ javascript/working-with-event-listeners/click/js/app.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+    let main = document.querySelector(`main`);
+    main.addEventListener(`click`, () => {
+        main.style.width = `500px`;
+        main.style.height = `500px`;
+    });
+};
