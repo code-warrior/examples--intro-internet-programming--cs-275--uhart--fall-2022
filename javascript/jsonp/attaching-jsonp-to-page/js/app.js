471cb948baec8cffc351c8b7462a24dda0c8dee9
Roy A Vanegas <roy@thecodeeducators.com>
2022-11-14 12:33:26 -0500

    Add JSONP example

--- javascript/jsonp/attaching-jsonp-to-page/js/app.js
+++ javascript/jsonp/attaching-jsonp-to-page/js/app.js
@@ -1,10 +1,13 @@
+function edbob(json) {
+    let message = document.getElementById(`msg`);
+
+    message.textContent = json[0].course;
+}
+
 window.onload = () => {
     let body = document.querySelector(`body`);
     let script = document.createElement(`script`);
+
     script.setAttribute(`src`, `json/data.json`);
     body.append(script);
-    // let b = document.createElement(`b`);
-    // let content = `This is content`;
-    // b.append(content);
-    // body.append(b);
 };
