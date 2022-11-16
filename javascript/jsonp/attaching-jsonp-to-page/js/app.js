1d72f51217e1debc6975b327088f28abb39348f2
Roy A Vanegas <roy@thecodeeducators.com>
2022-11-16 12:15:22 -0500

    Extend JSONP example

--- javascript/jsonp/attaching-jsonp-to-page/js/app.js
+++ javascript/jsonp/attaching-jsonp-to-page/js/app.js
@@ -1,7 +1,17 @@
 function edbob(json) {
     let message = document.getElementById(`msg`);
+    let randomDiv = document.getElementById(`random`);
+    let content = ``;
+    // message.textContent = json[0].course;
+    // randomDiv.textContent = `<i><b>${json[0].description}</b></i>`;
 
-    message.textContent = json[0].course;
+    console.log(`There are ${json.length} items in the JSON file`);
+    for(let i=0; i<json.length; i++) {
+        content += `<p>${json[i].course}</p>`;
+        console.log(json[i].thing.one);
+    }
+
+    randomDiv.innerHTML = content;
 }
 
 window.onload = () => {
